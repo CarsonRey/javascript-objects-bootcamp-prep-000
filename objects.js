15dce5c2fe4c0b875597cec14523fb1da03d7cbd
Carson Rey <c.rey494@gmail.com>
2018-08-16 03:00:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,10 @@
 var playlist = {
   artistName: 'songTitle'};
 
+var artistName = {
+  
+}
+
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign( {}, playlist, { [artistName]: songTitle})
   return playlist;
