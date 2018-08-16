9033b2e7021d5ea07a07eb05733ab15bd664740c
Carson Rey <c.rey494@gmail.com>
2018-08-16 03:03:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,6 @@
 var playlist = {
   artistName: 'songTitle'};
 
-var artistName = {
-  My Bloody Valentine:
-  Phil Ochs:
-  Slowdive:
-}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign( {}, playlist, { [artistName]: songTitle})
