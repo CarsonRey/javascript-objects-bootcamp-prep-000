8944dac1cfae9502405721263409fbbeb371252d
Carson Rey <c.rey494@gmail.com>
2018-08-16 03:35:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,8 +3,9 @@ var playlist = {
 
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle;
-  }
+  Object.assign( {}, playlist, { [artistName]: songTitle })
+  return playlist;
+}
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
